ab8346b2c86a6649ddbf12c6215ee76ee110c8ca
AsTro684 <110305574+AsTro684@users.noreply.github.com>
2022-09-21 16:42:55 -0500

    Add files via upload

---
+++ RiojasPrograma2/RiojasPrograma2/AccionesComunes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RiojasPrograma2
+{
+    internal class AccionesComunes
+    {
+        public static  void Mensaje(string mensaje)
+        {
+            MessageBox.Show(mensaje,"AVISO!!",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+        }
+        public static void LlenarCombo(string consulta, ComboBox combo, string id, string nombre)
+        {
+            combo.Items.Clear();
+            DataTable dt;
+           
+            dt =CargarDatos.EjecutarSeleccion(consulta);
+         //  dt.Rows.Add(0, "Mostrar TODOS");  
+            if (dt==null)
+            {
+                return;
+            }
+            DataRow dr = dt.NewRow();
+            dr[0] = 0;
+            dr[1] = "Mostrar Todos";
+            dt.Rows.InsertAt(dr, 0);
+
+
+            combo.DataSource = dt; 
+            combo.DisplayMember = nombre;
+            combo.ValueMember = id;
+            
+            
+
+
+        }
+    }
+}
---
+++ RiojasPrograma2/RiojasPrograma2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RiojasPrograma2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Mostrar todo");
+            string id = "id_departamento";
+            string nombre = "nombre_departamento";
+            int registrosafectados = 0;
+            registrosafectados = CargarDatos.EjecturaConsulta(textBox1.Text);
+            AccionesComunes.Mensaje("Registros Afectados" + registrosafectados);
+            AccionesComunes.LlenarCombo(textBox1.Text,comboBox1,id,nombre);
+            
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+            label1.Text = comboBox1.SelectedValue.ToString();
+        }
+    }
+}
